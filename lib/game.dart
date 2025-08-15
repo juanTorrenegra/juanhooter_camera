18b94bdf0aa698c9fe15e6e7c86dfc8a8d0511cf
juan <juantorrenegra2000@gmail.com>
2025-08-14 20:42:40 -0500

    cambio nombre instancia CameraComponet de _camera a camara

--- lib/game.dart
+++ lib/game.dart
@@ -13,16 +13,16 @@ class MyGame extends FlameGame with HasGameReference<MyGame> {
   late final Player player;
   late final GameHud hud;
   late final World universo;
-  CameraComponent? _camera;
+  CameraComponent? camara;
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
 
     universo = World();
-    _camera = CameraComponent(world: universo);
-    _camera!.viewfinder.anchor = Anchor.center;
-    _camera!.viewfinder.position = Vector2(size.x / 2, size.y / 2);
+    camara = CameraComponent(world: universo);
+    camara!.viewfinder.anchor = Anchor.center;
+    camara!.viewfinder.position = Vector2(size.x / 2, size.y / 2);
     final background = SpriteComponent(
       sprite: await Sprite.load('test.png'),
       size: Vector2(1600, 1600),
@@ -34,7 +34,7 @@ class MyGame extends FlameGame with HasGameReference<MyGame> {
     hud = GameHud()..priority = 10;
 
     add(universo);
-    add(_camera!);
+    add(camara!);
     add(hud);
 
     universo.add(background);
@@ -43,7 +43,7 @@ class MyGame extends FlameGame with HasGameReference<MyGame> {
 
   @override
   void onGameResize(Vector2 size) {
-    debugPrint('4. onGameResize (camera is $_camera)');
+    debugPrint('4. onGameResize (camera is $camara)');
     super.onGameResize(size);
   }
 
@@ -57,57 +57,3 @@ class MyGame extends FlameGame with HasGameReference<MyGame> {
     //print("se crea el bullet");
   }
 }
-
-
-
-//class MyGame extends FlameGame with HasGameReference<MyGame> {
-//  late final Player player;
-//  late final GameHud hud;
-//  late final World universo;
-//  CameraComponent? _camera;
-//
-//  @override
-//  Future<void> onLoad() async {
-//    //print("se inicia el on Load");
-//    await super.onLoad();
-//
-//    universo = World();
-//    //print("se crea el mundo");
-//    add(universo);
-//    //print("se añade el mundo");
-//
-//    _camera = CameraComponent(world: universo);
-//    //print("se crea la camara");
-//
-//    //_camera!.viewfinder.anchor = Anchor.topLeft;
-//    //_camera!.viewfinder.position = Vector2(size.x / 2, size.y / 2);
-//
-//    player = Player(
-//      sprite: await Sprite.load('ship.png'),
-//      position: Vector2(200, 200),
-//    );
-//    //print("se crea el player");
-//    world.add(player);
-//    //print("se añade el player");
-//
-//    final background = SpriteComponent(
-//      sprite: await Sprite.load('test.png'),
-//      size: Vector2(1600, 1600),
-//      //priority: -100,
-//    )..priority = -1;
-//    //print("se crea el background");
-//    //background.position = Vector2(size.x / 2, size.y / 2);
-//    //background.anchor = Anchor.topLeft;
-//    //background.position = Vector2(800, 800); // Centro del mundo (1600x1600)
-//    //print("se crea la posicion del background ");
-//    world.add(background);
-//    //print("se añade el background");
-//
-//    add(_camera!);
-//    //print("se añade la camara");
-//
-//    hud = GameHud();
-//    //print("se crea el hud");
-//    add(hud);
-//    //print("se añade el hud");
-//  }
